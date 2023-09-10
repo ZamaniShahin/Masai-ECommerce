317d221bf843166cc5531261d263cb0dca5f7d97
ZamaniShahin <sh.zamani1581@gmail.com>
2023-09-10 11:25:56 +0330

    ProductCategory Domain Done

--- 0_Framework/Domain/EntityBase.cs
+++ 0_Framework/Domain/EntityBase.cs
@@ -2,5 +2,11 @@
 
 public class EntityBase
 {
-    
+    public long Id { get; set; }
+    public DateTime CreationDate { get; set; }
+
+    public EntityBase()
+    {
+        CreationDate = DateTime.Now;
+    }
 }
