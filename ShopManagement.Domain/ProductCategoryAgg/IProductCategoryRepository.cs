6b06d32e1f346f71b811677e5272d3391d9d849c
ZamaniShahin <sh.zamani1581@gmail.com>
2023-09-10 11:29:21 +0330

    IProductCategoryRepository Done

---
+++ ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopManagement.Domain.ProductCategoryAgg;
+
+public interface IProductCategoryRepository
+{
+    void Create(ProductCategory entity);
+    ProductCategory Get(long id);
+    List<ProductCategory> GetAll();
+}
