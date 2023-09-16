1efdf989db5a60c7d8670d7bc1fd03642f4f71e0
ZamaniShahin <sh.zamani1581@gmail.com>
2023-09-16 14:02:43 +0330

    Application Contracts Done

---
+++ InventoryManagement.Application.Contracts/Inventory/IInventoryApplication.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+
+namespace InventoryManagement.Application.Contracts.Inventory
+{
+    public interface IInventoryApplication
+    {
+        OperationResult Create(CreateInventory command);
+        OperationResult Edit(EditInventory command);
+        OperationResult Increase(IncreaseInventory command);
+        OperationResult Reduce(ReduceInventory command);
+        OperationResult Reduce(List<ReduceInventory> command);
+        EditInventory GetDetails(long id);
+        List<InventoryViewModel> Search(InventorySearchModel searchModel);
+    }
+}
