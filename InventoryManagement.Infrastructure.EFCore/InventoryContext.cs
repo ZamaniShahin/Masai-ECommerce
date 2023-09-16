a4623851d4b1d2a2fe3149d5dfcbd27451942a6b
ZamaniShahin <sh.zamani1581@gmail.com>
2023-09-16 14:33:00 +0330

    InventoryContext Done

--- InventoryManagement.Infrastructure.EFCore/InventoryContext.cs
+++ InventoryManagement.Infrastructure.EFCore/InventoryContext.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InventoryManagement.Domain.InventoryAgg;
+using InventoryManagement.Infrastructure.EFCore.Mappings;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManagement.Infrastructure.EFCore
 {
     public class InventoryContext:DbContext
     {
+        public DbSet<Inventory> Inventory { get; set; }
         public InventoryContext(DbContextOptions<InventoryContext> options):base(options)
         {
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly =typeof(InventoryMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
