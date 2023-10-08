522374fe0a8b5665263083df345074978afc1c86
ZamaniShahin <sh.zamani1581@gmail.com>
2023-10-08 10:53:39 +0330

    Product Query Refactored

--- MasaiQuery/Contracts/Product/ProductQueryModel.cs
+++ MasaiQuery/Contracts/Product/ProductQueryModel.cs
@@ -17,22 +17,9 @@ namespace _01_MasaiQuery.Contracts.Product
         public string CategorySlug { get; set; }
         public bool HasDiscount { get; set; }
         public string DiscountExpireDate { get; set; }
-        public string Code { get; set; }
-        public string ShortDescription { get; set; }
         public string Slug { get; set; }
         public string Description { get; set; }
-        public string Keywords { get; set; }
-        public string MetaDescription { get; set; }
         public bool IsStock { get; set; }
         public List<ProductPictureQueryModel> Pictures { get; set; }
-    }
-
-    public class ProductPictureQueryModel
-    {
-        public long PictureId { get; set; }
-        public string Picture { get; set; }
-        public string PictureTitle { get; set; }
-        public string PictureAlt { get; set; }
-        public bool IsRemoved { get; set; }
     }
 }
