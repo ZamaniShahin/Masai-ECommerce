4ff14b3263a093d13216475376aebc18c1d82fc5
ZamaniShahin <sh.zamani1581@gmail.com>
2023-10-09 14:06:00 +0330

    Create Account And Edit Account Implemented

---
+++ AccountManagement.Application.Contracts/Account/CreateAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace AccountManagement.Application.Contracts.Account;
+
+public class CreateAccount
+{
+    public string FullName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Mobile { get; set; }
+    public string Email { get; set; }
+    public long RoleId { get; set; }
+    public string ProfilePhoto { get; set; }
+}
