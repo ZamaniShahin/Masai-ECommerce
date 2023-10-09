6a30ce598a8fa8e71c22e33bb34b5ad20ad8dfb5
ZamaniShahin <sh.zamani1581@gmail.com>
2023-10-09 14:25:18 +0330

    IAccountApplication IAccountRepository Implemented

--- AccountManagement.Domain/AccountAgg/IAccountRepository.cs
+++ AccountManagement.Domain/AccountAgg/IAccountRepository.cs
@@ -1,8 +1,10 @@
 ﻿using _0_Framework.Domain;
+using AccountManagement.Application.Contracts.Account;
 
 namespace AccountManagement.Domain.AccountAgg;
 
 public interface IAccountRepository : IRepository<long,Account>
 {
-    
+    EditAccount GetDetails(long id);
+    List<AccountViewModel> Search(AccountSearchModel searchModel);
 }
