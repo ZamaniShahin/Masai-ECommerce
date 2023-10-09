481346c49c34dafa7c013456db4fb3ea125a71bf
ZamaniShahin <sh.zamani1581@gmail.com>
2023-10-09 14:06:59 +0330

    AccountViewModel Implemented

---
+++ AccountManagement.Application.Contracts/Account/AccountViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AccountManagement.Application.Contracts.Account;
+
+public class AccountViewModel
+{
+    public string FullName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Mobile { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public string ProfilePhoto { get; set; }
+}
