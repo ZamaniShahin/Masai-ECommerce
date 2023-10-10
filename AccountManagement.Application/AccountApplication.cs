1c82d1342b5cbe8faed97946be3aae99bd443ed3
ZamaniShahin <sh.zamani1581@gmail.com>
2023-10-10 17:48:23 +0330

    AccountApplication All Implemented

--- AccountManagement.Application/AccountApplication.cs
+++ AccountManagement.Application/AccountApplication.cs
@@ -6,21 +6,52 @@ namespace AccountManagement.Application
 {
     public class AccountApplication : IAccountApplication
     {
+        private readonly IFileUploader _fileUploader;
+        private readonly IPasswordHasher _passwordHasher;
         private readonly IAccountRepository _accountRepository;
 
-        public AccountApplication(IAccountRepository accountRepository)
+        public AccountApplication(IAccountRepository accountRepository, IPasswordHasher passwordHasher, IFileUploader fileUploader)
         {
             _accountRepository = accountRepository;
+            _passwordHasher = passwordHasher;
+            _fileUploader = fileUploader;
         }
 
         public OperationResult Create(CreateAccount command)
         {
-            throw new NotImplementedException();
+            var operation = new OperationResult();
+
+            if (_accountRepository.Exists(x => x.UserName == command.UserName || command.Mobile == x.Mobile))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+            var password = _passwordHasher.Hash(command.Password);
+            var path = $"ProfilePhotos";
+            var picturePath = _fileUploader.Upload(command.ProfilePhoto, path);
+            var account = new Account(command.FullName, command.UserName
+                , password, command.Mobile,command.Email,command.RoleId,picturePath);
+            _accountRepository.Create(account);
+            _accountRepository.SaveChanges();
+
+            return operation.Succeeded();
         }
 
         public OperationResult Edit(EditAccount command)
         {
-            throw new NotImplementedException();
+            var operation = new OperationResult();
+
+            var account = _accountRepository.Get(command.Id);
+            if (account == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+            if (_accountRepository.Exists(x => x.UserName == command.UserName || 
+                                               command.Mobile == x.Mobile && 
+                                               x.Id == command.Id))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+            var path = $"ProfilePhotos";
+            var picturePath = _fileUploader.Upload(command.ProfilePhoto, path);
+            account.Edit(command.FullName, command.UserName,
+                command.Mobile, command.Email, command.RoleId, picturePath);
+            _accountRepository.SaveChanges();
+
+            return operation.Succeeded();
         }
 
         public OperationResult ChangePassword(ChangePassword command)
@@ -32,6 +63,11 @@ namespace AccountManagement.Application
             if (command.Password != command.Repassword)
                 return operation.Failed(ApplicationMessages.PasswordNotMatched);
             account.ChangePassword(command.Password);
+
+            var password = _passwordHasher.Hash(command.Password);
+            account.ChangePassword(password);
+            _accountRepository.SaveChanges();
+            return operation.Succeeded();
         }
 
         public EditAccount GetDetails(long id)
